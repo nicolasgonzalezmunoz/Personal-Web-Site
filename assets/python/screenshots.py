47840fccc1abab08829933834cfe34dc1887833d
nicolasgonzalezmunoz <nicolas.gonzalez.m97@gmail.com>
2024-01-24 16:39:13 -0300

    'Curriculum' tab updated, made screenshots script concurrent

--- assets/python/screenshots.py
+++ assets/python/screenshots.py
@@ -1,41 +1,19 @@
 import os
+import asyncio
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.by import By
 
-urls = [
-    # Current projects
-    "https://www.kaggle.com/code/nicolasgonzalezmunoz/underfitting-overfitting-and-regularization",
-    "https://www.kaggle.com/code/nicolasgonzalezmunoz/ml-the-statistician-way-basics/notebook",
-    "https://www.kaggle.com/code/nicolasgonzalezmunoz/cross-validation-dataset-split-strategies",
-    "https://www.kaggle.com/code/nicolasgonzalezmunoz/all-you-want-to-know-about-linear-regression",
-    "https://www.kaggle.com/datasets/nicolasgonzalezmunoz/earthquakes-on-chile",
-    "https://www.kaggle.com/datasets/nicolasgonzalezmunoz/world-bank-world-development-indicators"
-]
-screenshot_names = [
-    # Current projects
-    "overfitting-project.png",
-    "statistics-project.png",
-    "split-project.png",
-    "linear-project.png",
-    "overfitting-project.png",
-    "world-bank-dataset.png"
-]
-n_projects = len(urls)
 
-path = os.path.join('..', 'imgs')
+async def take_kaggle_screenshot(
+    url: str, filepath: os.PathLike, opts: FirefoxOptions
+    ) -> None:
 
-# Set browser options
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-
-for i in range(n_projects):
-    filepath = os.path.join(path, screenshot_names[i])
-    # Open browser
+    # Initialize navigator
     driver = webdriver.Firefox(options=opts)
 
-    # Navigate to site
-    driver.get(urls[i])
+    # Navigate to URL
+    driver.get(url)
 
     # Click on accept cookies
     driver.find_element(
@@ -52,3 +30,47 @@ for i in range(n_projects):
 
     # Exit browser
     driver.quit()
+
+
+async def take_kaggle_screenshots(
+    urls: list[str], filenames: list[str]
+        ) -> None:
+    n_projects: int = len(urls)
+    basepath: os.PathLike = os.path.join('..', 'imgs')
+
+    # Create set to save concurrent tasks
+    bg_tasks = set()
+
+    # Set browser options
+    opts = FirefoxOptions()
+    opts.add_argument("--headless")
+    for i in range(n_projects):
+        filepath = os.path.join(basepath, filenames[i])
+        task = asyncio.create_task(
+            take_kaggle_screenshot(urls[i], filepath, opts)
+        )
+        bg_tasks.add(task)
+        task.add_done_callback(bg_tasks.discard)
+    
+
+urls = [
+    # Current projects
+    "https://www.kaggle.com/code/nicolasgonzalezmunoz/underfitting-overfitting-and-regularization",
+    "https://www.kaggle.com/code/nicolasgonzalezmunoz/ml-the-statistician-way-basics/notebook",
+    "https://www.kaggle.com/code/nicolasgonzalezmunoz/cross-validation-dataset-split-strategies",
+    "https://www.kaggle.com/code/nicolasgonzalezmunoz/all-you-want-to-know-about-linear-regression",
+    "https://www.kaggle.com/datasets/nicolasgonzalezmunoz/earthquakes-on-chile",
+    "https://www.kaggle.com/datasets/nicolasgonzalezmunoz/world-bank-world-development-indicators"
+]
+
+filenames = [
+    # Current projects
+    "overfitting-project.png",
+    "statistics-project.png",
+    "split-project.png",
+    "linear-project.png",
+    "overfitting-project.png",
+    "world-bank-dataset.png"
+]
+
+asyncio.run(take_kaggle_screenshots(urls, filenames))
